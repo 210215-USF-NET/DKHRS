3be1db4e7c7f2de47f4793b964d25f1f34634122
Rich Hakes <rjhakes@yahoo.com>
2021-03-18 15:40:20 -0600

    Init project

---
+++ SHRKD_GivHub/GivHubDL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GivHubDL
+{
+    public class Class1
+    {
+    }
+}
---
+++ SHRKD_GivHub/GivHubModels/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GivHubModels
+{
+    public class Class1
+    {
+    }
+}
