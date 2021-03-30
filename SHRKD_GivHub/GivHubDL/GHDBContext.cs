e3415a7a5c04f6ac53c1139d6c7f787af0d19dce
Blanca Perez <hmittig21@gmail.com>
2021-03-30 13:17:53 -0400

    Added Follow table for social feature

--- SHRKD_GivHub/GivHubDL/GHDBContext.cs
+++ SHRKD_GivHub/GivHubDL/GHDBContext.cs
@@ -24,10 +24,10 @@ namespace GivHubDL
         public DbSet<SearchHistory> SearchHistories { get; set; }
         public DbSet<Donation> Donations { get; set; }
         public DbSet<Subscription> Subscriptions { get; set; }
+        public DbSet<Follow> Follows { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //modelBuilder.Entity<Location>().Property(x => x.Id).ValueGeneratedOnAdd();
 
             modelBuilder.Entity<Charity>().Property(x => x.Id).ValueGeneratedOnAdd();
             modelBuilder.Entity<Charity>()
@@ -42,6 +42,8 @@ namespace GivHubDL
             modelBuilder.Entity<SearchHistory>().Property(x => x.Id).ValueGeneratedOnAdd();
 
             modelBuilder.Entity<Subscription>().Property(x => x.Id).ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Follow>().Property(x => x.Id).ValueGeneratedOnAdd();
         }
     }
 }
--- SHRKD_GivHub/GivHubDL/Migrations/20210330171510_RelationshipFix.Designer.cs
+++ SHRKD_GivHub/GivHubDL/Migrations/20210330171510_RelationshipFix.Designer.cs
@@ -2,15 +2,17 @@
 using GivHubDL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace GivHubDL.Migrations
 {
     [DbContext(typeof(GHDBContext))]
-    partial class GHDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210330171510_RelationshipFix")]
+    partial class RelationshipFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
