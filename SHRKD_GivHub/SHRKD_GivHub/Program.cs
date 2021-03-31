d6343ce7aeca9dabdcfeab72f74d3b6251dc765b
hmittig <56450701+hmittig@users.noreply.github.com>
2021-03-31 16:36:46 -0400

    Revert "Steve branch"

--- SHRKD_GivHub/SHRKD_GivHub/Program.cs
+++ SHRKD_GivHub/SHRKD_GivHub/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,32 +13,11 @@ namespace SHRKD_GivHub
     {
         public static void Main(string[] args)
         {
-            var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(configuration).CreateLogger();
-            try 
-            {
-                Log.Information("Application starting up");
-                CreateHostBuilder(args).Build().Run();
-
-            }
-            catch (Exception ex) 
-            {
-                Log.Error(ex, "Application failed to start");
-                throw;
-            }
-            finally 
-            {
-                Log.CloseAndFlush();
-            }
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-            .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
